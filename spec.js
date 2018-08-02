6c33e58655269e9a9269e74bd956f3c0714de82c
jose.caitano <jose.caitano@virtus.ufcg.edu.br>
2018-08-02 15:54:58 -0300

    refactor

--- spec.js
+++ spec.js
@@ -1,8 +1,29 @@
-describe('angularjs homepage todo list', function() {
-  it('should add a todo', function() {
-    browser.get(helper.path());
-	element(by.name('usuario')).sendKeys('caitano');
-    element(by.name('senha')).sendKeys('123456');
-	element(by.id('botao')).click();
+var helper = require('./helper.js');
+
+describe('registro de formulário da página inicial', function() {
+	
+	var NameField = null,
+		PassField = null,
+		Btn = null,
+		alert_message = null;
+
+	
+	beforeEach(function () {
+        browser.get(helper.path());
+        browser.driver.manage().window().maximize();
+        NameField = element(by.id('name'));
+        PassField = element(by.id('password'));
+        Btn = element(by.id('botao'));
+    });
+	
+  it('write in the field user and password, then click ok', function() {
+    browser.sleep(5000);
+	NameField.sendKeys('admin');
+    PassField.sendKeys('admin123');
+    Btn.click();
+	browser.sleep(5000);
+	alert_message = element(by.id('alert_message'));
+	
+	expect(alert_message.getText()).toContain('Usuario: admin Senha: 123');
   });
 });
